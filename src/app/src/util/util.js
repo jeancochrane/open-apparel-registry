6361e808be485ac8a329b74747bc10c7a111ec3a
Kelly Innes <kinnes@azavea.com>
2019-01-16 10:24:50 -0500

    Replace CORS API headers with CRA proxy in dev
    - remove CORS headers setup from DRF API for development
    - set up Create React App server proxy for development
    - update client routes & tests
    
    Connects #46

--- src/app/src/util/util.js
+++ src/app/src/util/util.js
@@ -14,34 +14,33 @@ export function DownloadCSV(data, fileName) {
     }
 }
 
-export const makeUserLoginURL = () => `${process.env.REACT_APP_API_URL}/user-login/`;
-export const makeUserLogoutURL = () => `${process.env.REACT_APP_API_URL}/user-logout/`;
+export const makeUserLoginURL = () => '/user-login/';
+export const makeUserLogoutURL = () => `/user-logout/`;
 
 export const makeGetListsURL = uid =>
-    `${process.env.REACT_APP_API_URL}/getLists/${uid}/?key=${process.env.REACT_APP_API_KEY}`;
+    `/getLists/${uid}/?key=${process.env.REACT_APP_API_KEY}`;
 
 export const makeUpdateListURL = (uid, filename) =>
-    `${process.env.REACT_APP_API_URL}/getList/${uid}/?file_name=${filename}&key=${process.env.REACT_APP_API_KEY}`;
+    `/getList/${uid}/?file_name=${filename}&key=${process.env.REACT_APP_API_KEY}`;
 
 export const makeConfirmTempURL = tempId =>
-    `${process.env.REACT_APP_API_URL}/confirmTemp/${tempId}/?key=${process.env.REACT_APP_API_KEY}`;
+    `/confirmTemp/${tempId}/?key=${process.env.REACT_APP_API_KEY}`;
 
 export const makeUpdateSourceNameURL = uid =>
-    `${process.env.REACT_APP_API_URL}/updateSourceName/${uid}/?key=${process.env.REACT_APP_API_KEY}`;
+    `/updateSourceName/${uid}/?key=${process.env.REACT_APP_API_KEY}`;
 
 export const makeUploadTempFacilityURL = uid =>
-    `${process.env.REACT_APP_API_URL}/uploadTempFactory/${uid}/?key=${process.env.REACT_APP_API_KEY}`;
+    `/uploadTempFactory/${uid}/?key=${process.env.REACT_APP_API_KEY}`;
 
 export const makeGenerateAPIKeyURL = uid =>
-    `${process.env.REACT_APP_API_URL}/generateKey/${uid}/?key=${process.env.REACT_APP_API_KEY}`;
+    `/generateKey/${uid}/?key=${process.env.REACT_APP_API_KEY}`;
 
-export const makeAllSourceURL = () => `${process.env.REACT_APP_API_URL}/allsource/`;
-export const makeAllCountryURL = () => `${process.env.REACT_APP_API_URL}/allcountry/`;
-export const makeTotalFacilityURL = () => `${process.env.REACT_APP_API_URL}/totalFactories/`;
+export const makeAllSourceURL = () => `/allsource/`;
+export const makeAllCountryURL = () => `/allcountry/`;
+export const makeTotalFacilityURL = () => `/totalFactories/`;
 
 export const makeSearchFacilityByNameAndCountryURL = (name, country, contributor = null) => {
-    const baseURL =
-        `${process.env.REACT_APP_API_URL}/searchFactoryNameCountry/?name=${name}&country=${country}&contributor=`;
+    const baseURL = `/searchFactoryNameCountry/?name=${name}&country=${country}&contributor=`;
 
     return contributor
         ? baseURL.concat(contributor)
